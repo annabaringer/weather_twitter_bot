cdaadc2a1d8a2ccfb4adba0778be0bc42105a0d1
annabaringer <akobanna@gmail.com>
2021-12-29 16:11:34 -0700

    fix packages

--- twitter_app.py
+++ twitter_app.py
@@ -141,11 +141,11 @@ def get_photos(weather_main, city_name, country_name, api, logger):
     
 def main():
     # Set the environ variables
-    consumer_key = environ['consumer_key']
-    consumer_secret = environ['consumer_secret']
-    access_token = environ['access_token']
-    access_token_secret = environ['access_token_secret']
-    weather_api_key = environ['weather_api_key']
+    consumer_key = os.environ['consumer_key']
+    consumer_secret = os.environ['consumer_secret']
+    access_token = os.environ['access_token']
+    access_token_secret = os.environ['access_token_secret']
+    weather_api_key = os.environ['weather_api_key']
     
 
     ssl._create_default_https_context = ssl._create_unverified_context
